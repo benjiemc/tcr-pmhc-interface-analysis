47d9fafb039a3140117060234d25b1ad3d569c32
Benjamin McMaster <benjamin.mcmaster@rdm.ox.ac.uk>
2024-05-17 16:33:01 +0100

    Add summary notebook

--- src/tcr_pmhc_interface_analysis/utils.py
+++ src/tcr_pmhc_interface_analysis/utils.py
@@ -1,5 +1,10 @@
+import logging
+import re
+
 import pandas as pd
 from python_pdb.formats.residue import THREE_TO_ONE_CODE
+
+logger = logging.getLogger(__name__)
 
 
 def get_coords(df):
@@ -24,3 +29,45 @@ def get_header(pdb_contents: str) -> str:
         header.append(line)
 
     return '\n'.join(header)
+
+
+def mhc_code_to_slug(code: str) -> str:
+    '''Convert MHC allele codes into slugs.
+
+    >>> mhc_code_to_slug('HLA-A*02:01:59')
+    'hla_a_02_01_59'
+
+    '''
+    slug = code.lower()
+    slug = re.sub(r'[*:-]', '_', slug)
+
+    return slug
+
+
+def mhc_slug_to_code(slug: str) -> str:
+    '''Convert mhc slugs into the allele codes.
+
+    >>> mhc_slug_to_code('hla_a_02_01')
+    'HLA-A*02:01'
+
+    >>> mhc_slug_to_code('h2_kb')
+    'H2-Kb'
+
+    TODO: Make this better for other mouse alleles
+    '''
+    species = 'human' if slug.startswith('hla') else 'mouse' if slug.startswith('h2') else None
+
+    if species == 'human':
+        code = slug.upper()
+        code = code.split('_')
+        code = code[0] + '-' + code[1] + '*' + ':'.join(code[2:])
+
+    elif species == 'mouse':
+        code = slug.title()
+        code = code.replace('_', '-')
+
+    else:
+        logger.error('Species not found, outputting slug')
+        code = slug
+
+    return code
